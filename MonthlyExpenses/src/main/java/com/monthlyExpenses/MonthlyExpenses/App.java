1092d525ebd436b644154d3201c830e35b24839e
rajeev <rajeev@rajeev-HP-Pavilion-15-Notebook-PC>
2017-09-19 23:22:12 +0530

    First commit

---
+++ MonthlyExpenses/src/main/java/com/monthlyExpenses/MonthlyExpenses/App.java
@@ -0,0 +1,13 @@
+package com.monthlyExpenses.MonthlyExpenses;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
